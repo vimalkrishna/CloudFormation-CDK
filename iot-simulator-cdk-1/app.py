3f23a73c31a62df536431fcd9dee89d9fa344721
Vimal Krishna <krishnavimal@gmail.com>
2025-04-18 17:07:21 +0200

    Creation of S3, SQS, CDK Bootstrap and CDK Deploy!

--- iot-simulator-cdk-1/app.py
+++ iot-simulator-cdk-1/app.py
@@ -2,27 +2,31 @@
 import os
 
 import aws_cdk as cdk
-
+from aws_cdk import App, Environment
+# Folder: iot_simulator_cdk_1
+# File: iot_simulator_cdk_1/iot_simulator_cdk_1_stack.py
+# Import the necessary class from the above file
 from iot_simulator_cdk_1.iot_simulator_cdk_1_stack import IotSimulatorCdk1Stack
 
+# account and region defined in the environment
+env_aws =Environment(account='327625635979', region='eu-central-1')
+'''
+App is the main entry point for the CDK application.
+App as the root: The App object is the entry point for defining your cloud infrastructure. It's the root construct of your CDK application, representing the entire deployment.
 
+Construct tree: It acts as the base of the construct tree where all other resources (like stacks, constructs, etc.) are added. Every stack or resource you define must be attached to this app instance.
+
+Synthesis: During the synthesis phase, cdk.App() generates the necessary CloudFormation templates based on the constructs defined in your application.
+This is crucial for deploying your infrastructure as code, ensuring that your resources are created in a consistent and repeatable manner.
+# Environment: The environment parameter allows you to specify the AWS account and region where the stack will be deployed. This is useful for multi-account or multi-region deployments.
+'''
 app = cdk.App()
-IotSimulatorCdk1Stack(app, "IotSimulatorCdk1Stack",
-    # If you don't specify 'env', this stack will be environment-agnostic.
-    # Account/Region-dependent features and context lookups will not work,
-    # but a single synthesized template can be deployed anywhere.
-
-    # Uncomment the next line to specialize this stack for the AWS Account
-    # and Region that are implied by the current CLI configuration.
-
-    #env=cdk.Environment(account=os.getenv('CDK_DEFAULT_ACCOUNT'), region=os.getenv('CDK_DEFAULT_REGION')),
-
-    # Uncomment the next line if you know exactly what Account and Region you
-    # want to deploy the stack to. */
-
-    #env=cdk.Environment(account='123456789012', region='us-east-1'),
-
-    # For more information, see https://docs.aws.amazon.com/cdk/latest/guide/environments.html
-    )
-
-app.synth()
+#Stack definition, we can add many stacks here
+s3_stack = IotSimulatorCdk1Stack(app, "IotSimulatorCdk1Stack", env=env_aws)
+app.synth() 
+'''
+The synth() method is responsible for generating the CloudFormation templates based on the defined stacks and constructs. It translates your high-level CDK code into a low-level representation that AWS CloudFormation can understand.
+Here, app.synth() generates the CloudFormation templates for the resources added under the App instance. It's the backbone of your CDK application!
+The synth() method is called at the end of your CDK application to produce the CloudFormation templates that will be deployed to AWS.
+The generated templates can be found in the cdk.out directory after running the application.
+'''
